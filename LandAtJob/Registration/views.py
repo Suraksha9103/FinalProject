ac63c9df6f19ea0953f05b404d3398f09f96a9fa
Surasha9103 <suraksha9103@gmail.com>
2023-03-18 12:27:18 +0545

    SignUp Form Create

--- LandAtJob/Registration/views.py
+++ LandAtJob/Registration/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def loginform(request):
+    data={
+        'title':'Member Registration'
+
+    }
+    return render(request,"registration/login.html",data)
