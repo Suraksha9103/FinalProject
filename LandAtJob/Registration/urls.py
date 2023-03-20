754756b5946e1b5a41501d0eff791f9dfdf31f82
Surasha9103 <suraksha9103@gmail.com>
2023-03-20 08:50:19 +0545

    registration complete

--- LandAtJob/Registration/urls.py
+++ LandAtJob/Registration/urls.py
@@ -5,4 +5,6 @@ from . import views
 
 urlpatterns = [
     path('',views.loginform, name="registration.login"),
+    path('signup/',views.signform,name="registration.signup"),
+    path('store/',views.registrationStore,name="registration.store")
 ] 
--- LandAtJob/Registration/views.py
+++ LandAtJob/Registration/views.py
@@ -1,10 +1,42 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import redirect, render
+
+from Registration.models import Registration
 
 # Create your views here.
 
-def loginform(request):
+def loginform(request): 
     data={
         'title':'Member Registration'
 
     }
     return render(request,"registration/login.html",data)
+
+def signform(request):
+    date={
+        'title': 'signup'
+    }
+    return render(request,"Registration/signup.html",date)
+
+def registrationStore(request):
+    if request.method=="POST":
+        password=request.POST['password']
+        cpassword=request.POST['cpassword']
+
+    if password != cpassword:
+        return HttpResponse("Your Password Does not Match") 
+
+    else:
+        registration=Registration(
+            email= request.POST['email'],
+            full_name= request.POST['full_name'],
+            dob =request.POST['dob'],
+            password= request.POST['password'],
+            cpassword= request.POST['cpassword'],
+            created_at= request.POST.get('created_at', False),
+            updated_at= request.POST.get('updated_at', False)
+        )
+        registration.save()
+        return redirect('registration.login')
+    return render(request,'registration/signup.html')
+            
